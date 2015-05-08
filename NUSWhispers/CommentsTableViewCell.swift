0f8570c6882c1289a18ea8be0e2a82d8e3923962
Jingwen Chen <jin@crypt.sg>
2015-05-09 00:02:49 +0800

    Reorganized Xcode project structure. Added skeleton for CommentsTableViewController.

---
+++ NUSWhispers/CommentsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentsTableViewCell.swift
+//  NUSWhispers
+//
+//  Created by jin on 9/5/15.
+//  Copyright (c) 2015 crypt. All rights reserved.
+//
+
+import UIKit
+
+class CommentsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
