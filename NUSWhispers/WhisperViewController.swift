9379e953320786fced5b53b2bec8758892f1484d
Jingwen Chen <jin@crypt.sg>
2015-05-11 00:14:42 +0800

    Made the whisper modal a page sheet. Added gesture for tapping anywhere to dismiss modal view.

--- NUSWhispers/WhisperViewController.swift
+++ NUSWhispers/WhisperViewController.swift
@@ -18,7 +18,13 @@ class WhisperViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        whisperContentAttributedLabel.enabledTextCheckingTypes = NSTextCheckingType.Link.rawValue
+        whisperContentAttributedLabel.userInteractionEnabled = true
         whisperContentAttributedLabel.text = whisper?.content
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        setUpDismissGesture()
     }
 
     override func viewDidLayoutSubviews() {
@@ -29,9 +35,23 @@ class WhisperViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func closeButtonWasPressed(sender: AnyObject) {
-        self.dismissViewControllerAnimated(true, completion: nil)
+
+    private func setUpDismissGesture() {
+        let recognizer = UITapGestureRecognizer(target: self, action: "dismissViewController:")
+        recognizer.numberOfTapsRequired = 1
+        recognizer.cancelsTouchesInView = false
+        self.view.window?.addGestureRecognizer(recognizer)
+    }
+
+    func dismissViewController(sender: UITapGestureRecognizer) {
+        if sender.state == .Ended {
+            let location = sender.locationInView(nil)
+            if self.view.pointInside(
+                self.view.convertPoint(location, fromView: self.view.window), withEvent: nil) {
+                    self.view.window?.removeGestureRecognizer(sender)
+                    self.dismissViewControllerAnimated(true, completion: nil)
+            }
+        }
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
